916fe77c566d7c46679c53ee58329283090ea0ed
Dariusz Filipiak <darekf77@gmail.com>
2024-12-06 02:15:52 +0100

    $$$ update $$$

--- src/lib/project/features/inside-structures/structs/inside-struct-helpers.ts
+++ src/lib/project/features/inside-structures/structs/inside-struct-helpers.ts
@@ -231,9 +231,9 @@ ${'//#reg' + 'ion'} @${'bro' + 'wser'}
   providedIn:'root'
 })
 export class UserApiService {
-  userControlller = Taon.inject(()=> MainContext.getClass(UserController))
+  userController = Taon.inject(()=> MainContext.getClass(UserController))
   getAll() {
-    return this.userControlller.getAll()
+    return this.userController.getAll()
       .received
       .observable
       .pipe(map(r => r.body.json));
@@ -285,7 +285,7 @@ var MainContext = Taon.createContext(()=>({
   contexts:{ BaseContext },
   controllers: {
     UserController,
-    // PUT FIREDEV CONTORLLERS HERE
+    // PUT FIREDEV CONTROLLERS HERE
   },
   entities: {
     User,
