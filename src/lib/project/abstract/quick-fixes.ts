ba8d5737259a9c00df4f2a74188270a874712249
Dariusz Filipiak <darekf77@gmail.com>
2025-08-12 17:44:49 +0200

    feat: app folder md info

--- src/lib/project/abstract/quick-fixes.ts
+++ src/lib/project/abstract/quick-fixes.ts
@@ -332,6 +332,30 @@ ${THIS_IS_GENERATED_STRING}
       })();
     }
 
+    const app_folder_path = crossPlatformPath([
+      this.project.location,
+      config.folder.src,
+      'app',
+      'app-folder-info.md',
+    ]);
+
+    Helpers.writeFile(
+      app_folder_path,
+      `${THIS_IS_GENERATED_STRING}
+
+# HOW TO USE THIS FOLDER
+
+
+Put here files that you don't want to share through npm package.
+
+*src/lib* - it is for **npm library** code
+
+*src/app* - it is for **app only** code
+(app -> means here: webapp, nodejs server, electron, vscode plugin etc.)
+
+${THIS_IS_GENERATED_STRING}`,
+    );
+
     Helpers.taskDone(`[quick fixes] missing angular lib fles end`);
     //#endregion
   }
