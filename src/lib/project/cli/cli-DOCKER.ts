92d99fb8b58cb53b0ff8cfb5706afb16f062d830
Dariusz Filipiak <darekf77@gmail.com>
2025-08-15 19:10:09 +0200

    $$$ update $$$

--- src/lib/project/cli/cli-DOCKER.ts
+++ src/lib/project/cli/cli-DOCKER.ts
@@ -13,6 +13,25 @@ class $Docker extends BaseCli {
     console.log(`Hello from taon Docker CLI!`);
     this._exit();
   }
+
+  transmission() {
+    Helpers.run(
+      `docker run -d \`
+  --name transmission \`
+  -p 9091:9091 \`
+  -p 51413:51413 \`
+  -p 51413:51413/udp \`
+  -e TZ=Europe/Warsaw \`
+  -v "$env:USERPROFILE\\Downloads:/downloads" \`
+  -v "$env:USERPROFILE\\transmission-config:/config" \`
+  --restart unless-stopped\`
+  linuxserver/transmission:latest
+
+      `,
+      { output: true },
+    ).sync();
+    this._exit();
+  }
 }
 
 export default {
