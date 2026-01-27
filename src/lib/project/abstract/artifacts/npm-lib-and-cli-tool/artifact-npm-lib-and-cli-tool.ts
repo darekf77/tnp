4134f6a242088c0a929570ed4fad21c50b31086d
Dariusz Filipiak <darekf77@gmail.com>
2026-01-28 00:54:13 +0100

    chore: update

--- src/lib/project/abstract/artifacts/npm-lib-and-cli-tool/artifact-npm-lib-and-cli-tool.ts
+++ src/lib/project/abstract/artifacts/npm-lib-and-cli-tool/artifact-npm-lib-and-cli-tool.ts
@@ -1644,7 +1644,7 @@ ${THIS_IS_GENERATED_INFO_COMMENT}
       destStartJS,
       `console.log('<<< USING BUNDLED CLI >>>');\n` +
         `global.taonUsingBundledCliMode = true;\n` +
-        `global.frameworkName = ${this.project.nameForCli};` +
+        `global.frameworkName = "${this.project.nameForCli}";` +
         `\n${Helpers.readFile(destStartJS)}`,
     );
 
