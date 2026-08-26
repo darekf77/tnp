659d257954f85cc66f51877b29643a4de5d636a4
Dariusz Filipiak <darekf77@gmail.com>
2026-08-26 14:39:24 +0200

    chore: tests folder desc

--- src/lib/project/abstract/quick-fixes.ts
+++ src/lib/project/abstract/quick-fixes.ts
@@ -334,46 +334,15 @@ ${THIS_IS_GENERATED_STRING}
 ${THIS_IS_GENERATED_STRING}
 
 # Purpose of this folder
-Put your backend **mocha** tests (with *.test.ts extension) in this folder or any other *tests*
-folder inside project.
-
-\`\`\`
-/src/lib/my-feature/features.test.ts                          # -> NOT ok, test omitted
-/src/lib/my-feature/tests/features.test.ts                    # -> OK
-/src/lib/my-feature/nested-feature/tests/features.test.ts     # -> OK
-\`\`\`
+Put your backend **vite** tests (with *.test.ts extension) in this folder..
 
 # How to test your isomorphic backend ?
 
-1. By using console select menu:
-\`\`\`
-taon test                   # single run
-taon test:watch             # watch mode
-taon test:debug             # and start "attach" VSCode debugger
-taon test:watch:debug       # and start "attach" VSCode debugger
-\`\`\`
+- open file in vscode
+- Select "Debug And Run" icon
+- Select "Run Vitest (editor active test file)"
 
-2. Directly:
-\`\`\`
-taon mocha                        # single run
-taon mocha:watch                  # watch mode
-taon mocha:debug                  # and start "attach" VSCode debugger
-taon mocha:watch:debug            # and start "attach" VSCode debugger
-\`\`\`
-
-# Example
-example.test.ts
-\`\`\`ts
-import { describe, before, it } from 'mocha'
-import { expect } from 'chai';
-
-describe('Set name for function or class', () => {
-
-  it('should keep normal function name ', () => {
-    expect(1).to.be.eq(Number(1));
-  })
-});
-\`\`\`
+ps. you can create .test.ts files anywhere, but this folder is just for tests.
 
 ${THIS_IS_GENERATED_STRING}
 
