fda4d1c81264fe1de239ca3697c8470ff2e0c4ad
Dariusz Filipiak <darekf77@gmail.com>
2026-09-20 18:01:57 +0200

    fix: push only for v21 and up

--- src/lib/project/abstract/git.ts
+++ src/lib/project/abstract/git.ts
@@ -71,13 +71,15 @@ export class Git extends BaseGit<Project> {
 
     this.project.quickFixes.removeHuskyHooks();
 
-    while (!(await this.project.secretEnv.canChangesBePush())) {
-      Helpers.info(`
+    if (this.project.framework.frameworkVersionAtLeast('v21')) {
+      while (!(await this.project.secretEnv.canChangesBePush())) {
+        Helpers.info(`
 
         ${chalk.bold.red('BEFORE PUSHING PLEASE ALWAYS ENCRYPT ALL YOUR DATA')}
 
         `);
-      await this.project.secretEnv.encode();
+        await this.project.secretEnv.encode();
+      }
     }
 
     //#endregion
